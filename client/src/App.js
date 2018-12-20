788ded85b3a0b2b9a2bff1b9710bdc4825974c60
ystasiuk <>
2018-12-20 02:03:53 +0200

    Client #1: Basic component updation

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'The Troops of St. Tropez',
+      description: 'Gendarme Ludovic Cruchot is re-assigned to the French Riviera seaside resort town of Saint-Tropez where petty criminals and his own daughter give him a hard time.',
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="film-container">
+          <div className="film-block">
+            <h2>
+              {this.state.name}
+            </h2>
+            <div>
+              {this.state.description}
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
